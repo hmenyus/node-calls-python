d07594f0b083f4f5c5b0a9583edfab1b9e4df773
Menyhért Hegedűs <30656151+hmenyus@users.noreply.github.com>
2022-07-18 23:42:44 +0200

    Rpath (#19)
    * rpath
    
    * rpath
    
    * simplify script

--- scripts/rpaths.js
+++ scripts/rpaths.js
@@ -8,20 +8,8 @@ if (stdout)
     const result = [];
     splits.forEach(s => {
         if (s.startsWith("-L"))
-            result.push(s.substring(2));
+            result.push("-Wl,-rpath," + s.substring(2));
     });
 
-    result.forEach((r, i) => {
-        if (i == 0)
-        {
-            if (result.length > 1)
-                console.log("-Wl,-rpath," + r +" \\");
-            else
-                console.log("-Wl,-rpath," + r);
-        }
-        else if (i + 1 == result.length)
-            console.log("\t-Wl,-rpath," + r);
-        else
-            console.log("\t-Wl,-rpath," + r + " \\");
-    });
+    console.log(result.join(" "));
 }
