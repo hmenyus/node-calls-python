7f4c6f6ae457096b3cbb82e20964fb418dc1c657
Menyhért Hegedűs <30656151+hmenyus@users.noreply.github.com>
2024-04-22 10:31:22 +0200

    Esm (#70)
    * em module
    
    * bump version
    
    * fix destrcution

---
+++ test/test.mjs
@@ -0,0 +1,13 @@
+import { interpreter as py } from 'node-calls-python'
+import {join, dirname } from 'path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+let pyfile = join(__dirname, "nodetest.py");
+
+let pymodule = py.importSync(pyfile);
+
+console.log(py.callSync(pymodule, "hello"));
+console.log(py.callSync(pymodule, "concatenate", "aaa", "bbb"));
